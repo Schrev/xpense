807745ce2a500820a41cb4f80b583a3bbad53ff1
Ruy Schneider <ruy.schneider@icloud.com>
2022-01-12 15:03:19 -0600

    separation of concerns

--- xpense.rb
+++ xpense.rb
@@ -1,11 +1,13 @@
 require 'csv'
+require_relative 'methods.rb'
 
 puts "-->      Xpense-Report"
 puts "-->      12/01/2022"
 puts "______________________________________________________"
 puts "Please write the path to your report (.csv): "
-
 path = gets.chomp
+puts "______________________________________________________"
+
 transportation = []
 meals = []
 parking = []
@@ -37,59 +39,10 @@ puts ' '
 puts "______________________________________________________"
 puts ' '
 
-def transportxpense(transportation)
-  ttotal = 0
-  transportation.each do |kms|
-    ttotal += kms.to_i
-  end
-  if ttotal <= 100
-    ttotal = ttotal * 0.12
-  else
-    ttotal = (ttotal - 100) * 0.08 + 12
-  end
-  ttotal
-end
-
-def mealsxpense(meals)
-  mtotal = 0
-  if meals.size <= 3
-    mtotal = meals.size.to_i * 10
-  else
-    mtotal = ((meals.size.to_i - 3) * 6) + 30
-  end
-  mtotal
-end
-
-def parkingxpense(parking)
-  ptotal = 0
-  parking.each do |eurs|
-    ptotal += eurs.to_i
-  end
-  if ptotal <= 20
-    ptotal = ptotal
-  else
-    ptotal = ((ptotal - 20) * 0.5) + 20
-  end
-  ptotal
-end
-
-def otherxpense(others)
-  ototal = 0
-  others.each do |other|
-    ototal += other.to_i
-  end
-  ototal
-end
-
 ttotal = transportxpense(transportation)
 mtotal = mealsxpense(meals)
 ptotal = parkingxpense(parking)
 ototal = otherxpense(others)
-
-def refund(ttotal, mtotal, ptotal)
-  total = ttotal + mtotal + ptotal
-end
-
 total = refund(ttotal, mtotal, ptotal)
 
 puts ' '
